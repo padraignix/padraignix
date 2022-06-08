50fe81fe57616d187fd80f6c44a8064460f0df32
Patrick Downing <19316277+padraignix@users.noreply.github.com>
2022-06-07 20:57:43 -0400

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -30,12 +30,13 @@ def fetch_blog_entries():
 
 if __name__ == "__main__":
     readme = root / "README.md"
+    readme_contents = readme.open().read()
     
     entries = fetch_blog_entries()
     entries_md = "| Post | Date |\n| --------------------------------------- | -------------- |".join(
         ["| [{title}]({url}) | {published} |".format(**entry) for entry in entries]
     )
-    rewritten = replace_chunk(rewritten, "blog", entries_md)
+    rewritten = replace_chunk(readme_contents, "blog", entries_md)
 
     readme.open("w").write(rewritten)
   
